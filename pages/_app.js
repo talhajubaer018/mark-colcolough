64f0e9a4379975ef279831e1fe1c4511ed2a17c4
talhajubaer018 <52033279+talhajubaer018@users.noreply.github.com>
2021-10-04 23:47:09 +0600

    fontawesome added

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
 import '../styles/globals.css'
 import 'tailwindcss/tailwind.css'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+import { faCoffee } from '@fortawesome/free-solid-svg-icons'
 
 import Layout from '../components/Layout'
+
+library.add(fab, faCoffee)
 
 function MyApp({ Component, pageProps }) {
   return (
