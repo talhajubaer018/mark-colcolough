f082551acf7fc93418be45843714b179a68ef523
talhajubaer018 <52033279+talhajubaer018@users.noreply.github.com>
2021-10-04 23:15:13 +0600

    tailwind config added

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,22 @@ module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        customYellow: {
+          500: '#C3872C',
+        },
+        customTeal: {
+          500: '#62C8D8'
+        },
+      },
+      gridTemplateColumns: {
+        '1/2': '1fr 2fr'
+      },
+    }
   },
   variants: {
-    extend: {},
+    extend: {}
   },
-  plugins: [],
+  plugins: []
 }
