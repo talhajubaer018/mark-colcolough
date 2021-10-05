56636226fea5c20343cd04a0aa6c6cae2d6da3af
talhajubaer018 <52033279+talhajubaer018@users.noreply.github.com>
2021-10-05 18:08:04 +0600

    navbar key added

--- components/Navbar.js
+++ components/Navbar.js
@@ -6,8 +6,8 @@ const Navbar = ({ navItems }) => {
   return (
     <nav className='flex justify-between place-items-center text-center w-3/4 text-customYellow-500'>
       {navItems.map(item => (
-        <Link href={`/${item.title.replace(/\?/g, '').replace(/\s/g, '').toLowerCase()}`}>
-          <h3 key={item.id} className='hover:text-customTeal-500 cursor-pointer'>
+        <Link key={item.id} href={`/${item.title.replace(/\?/g, '').replace(/\s/g, '').toLowerCase()}`}>
+          <h3 className='hover:text-customTeal-500 cursor-pointer'>
             {item.title}
           </h3>
         </Link>
