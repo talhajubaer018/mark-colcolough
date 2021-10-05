dd4a4f1d96a124e81974fbb4f6b9da0fcb926d0a
talhajubaer018 <52033279+talhajubaer018@users.noreply.github.com>
2021-10-05 17:04:01 +0600

    footer fixed

--- components/Footer.js
+++ components/Footer.js
@@ -1,27 +1,42 @@
 import React from 'react'
+import Link from 'next/link'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 const Footer = () => {
   return (
-    <footer className='grid grid-cols-3 place-items-center bg-customTeal-200 py-8'>
-      <div>
-        <h5>Mark Colcolough</h5>
-        <h5>ShareThisRide ApS</h5>
-        <h5>Smørmosetoften 8</h5>
-        <h5>5700 Svendborg</h5>
-        <h5>CVR : DK38285424</h5>
+    <footer>
+      <div className='grid grid-cols-3 place-items-center bg-customTeal-200 py-8'>
+        <div className='font-light text-center'>
+          <h4 className='font-bold'>Mark Colcolough</h4>
+          <h4>ShareThisRide ApS</h4>
+          <h4>Smørmosetoften 8</h4>
+          <h4>5700 Svendborg</h4>
+          <h4>CVR : DK38285424</h4>
+        </div>
+        <div className='text-customTeal-500 text-center'>
+          <h4 className='mb-4'>+4561488488</h4>
+          <h4>hello@markcolclough.dk</h4>
+        </div>
+        <div className='flex gap-x-8 text-customGray-700 fa-2x'>
+          <div>
+            <FontAwesomeIcon className='' icon={['fab', 'facebook']} />
+          </div>
+          <div>
+            <FontAwesomeIcon className='' icon={['fab', 'instagram']} />
+          </div>
+          <div>
+            <FontAwesomeIcon className='' icon={['fab', 'linkedin-in']} />
+          </div>
+        </div>
+      </div>
+      <div className='grid grid-cols-3 place-items-center bg-customTeal-200 pb-8'>
         <h6>© Copyright, 2021</h6>
+        <h6></h6>
+        <Link href='/privacy'>
+          <h4 className='text-customTeal-500 hover:text-customYellow-500 cursor-pointer'>Privacy & Data Policy</h4>
+        </Link>
       </div>
-      <div className='text-customTeal-500'>
-        <h5>+4561488488</h5>
-        <h5>hello@markcolclough.dk</h5>
-      </div>
-      <div className='text-customTeal-500'>
-        <FontAwesomeIcon className='' icon={['fab', 'facebook']} />
-        <FontAwesomeIcon className='' icon={['fab', 'instagram-square']} />
-        <FontAwesomeIcon className='' icon={['fab', 'linkedin-in']} />
-        <h4>Privacy & Data Policy</h4>
-      </div>
+
     </footer>
   )
 }
