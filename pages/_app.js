e81b2a1d353b4a4fac0cb255722bf1da847cbabe
talhajubaer018 <52033279+talhajubaer018@users.noreply.github.com>
2021-10-05 21:59:28 +0600

    fontawesome icon added to _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -2,11 +2,11 @@ import '../styles/globals.css'
 import 'tailwindcss/tailwind.css'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { fab } from '@fortawesome/free-brands-svg-icons'
-import { faCoffee } from '@fortawesome/free-solid-svg-icons'
+import { fas, faLongArrowAltRight } from '@fortawesome/free-solid-svg-icons'
 
 import Layout from '../components/Layout'
 
-library.add(fab, faCoffee)
+library.add(fab, fas, faLongArrowAltRight)
 
 function MyApp({ Component, pageProps }) {
   return (
