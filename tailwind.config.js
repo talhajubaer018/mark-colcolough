9fb90dcea1d57a3ba73ca8aeeecabde7bec279f4
talhajubaer018 <52033279+talhajubaer018@users.noreply.github.com>
2021-10-20 21:44:10 +0600

    tailwind config added

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,24 +9,37 @@ module.exports = {
         },
         customTeal: {
           200: '#E4EEF1',
+          300: '#E0F1FB',
           500: '#62C8D8'
         },
         customGray: {
+          200: '#F3F3F3',
+          300: '#F8F9FB',
+          400: '#7B7B7B',
           700: '#707070',
           900: '#4B4B4B'
+        },
+        customLavender: {
+          300: '#EEECFF'
         }
       },
       fontSize: {
+        10: '0.625rem !important',
+        11: '0.6875rem !important',
+        12: '0.75rem !important',
         14: '0.875rem !important',
         15: '0.9375rem !important',
         16: '1rem !important',
         18: '1.125rem !important',
         20: '1.25rem !important',
         25: '1.5625rem !important',
+        30: '1.875rem !important',
         40: '2.5rem !important',
+        50: '3.125rem !important',
       },
       gridTemplateColumns: {
-        '1/2': '1fr 2fr'
+        '1/2': '1fr 2fr',
+        '3/2': '3fr 2fr',
       },
       spacing: {
         '10': '10%',
@@ -34,6 +47,14 @@ module.exports = {
       },
       boxShadow: {
         custom: 'rgba(14, 30, 37, 0.12) 0px 2px 4px 0px, rgba(14, 30, 37, 0.32) 0px 2px 16px 0px;'
+      },
+      width: {
+        '5p': '5%',
+        '15p': '15%',
+      },
+      maxWidth: {
+        '3/4': '75%',
+        '9/10': '90%',
       }
     }
   },
