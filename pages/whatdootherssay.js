34f2c49bbd94906e235a799d0b787cc6ff54d381
talhajubaer018 <52033279+talhajubaer018@users.noreply.github.com>
2021-10-22 23:36:41 +0600

    what do others say page fix

--- pages/whatdootherssay.js
+++ pages/whatdootherssay.js
@@ -28,32 +28,34 @@ const whatdootherssay = ({  }) => {
   }, [])
 
   return (
-    <div className='py-8'>
-      <h1 className='text-center mb-16 text-customYellow-500'>What Do Others Say</h1>
-      <div className='grid grid-cols-3 place-items-center sm:gap-x-4 sm:gap-y-16 lg:gap-x-16 lg:gap-y-24'>
-        {quotes.map(item => (
-          <div className='shadow-custom rounded-lg p-4'>
-            <div className='relative head' style={{paddingLeft: iconWidth}}>
-              <img className='absolute top-0 left-0 transform -translate-y-1/2 icon-quote w-15 lg:w-15' src='/icons/icon-quote.png' />
-              <h4 className='font-bold'>
-                {item.name}
-              <span className='block font-light text-16'>age {item.age}</span>
-              </h4>
-            </div>
-            <div className='my-4'>
-              <h4 className='mb-4'>{item.text1}</h4>
-              <h4 className='mb-4'>{item.text2}</h4>
-              <Link href='/whatdootherssay/[id]' as={`/whatdootherssay/${item.id}`}>
-                <h4 className='text-customTeal-500 hover:text-customYellow-500 cursor-pointer'>
-                  Read More
-                  <FontAwesomeIcon className='ml-2' icon={['fas', 'long-arrow-alt-right']} />
+    <section className='py-8'>
+      <div className='container mx-auto'>
+        <h1 className='text-center mb-16 text-customYellow-500'>What Do Others Say</h1>
+        <div className='grid grid-cols-3 place-items-center sm:gap-x-4 sm:gap-y-16 lg:gap-x-16 lg:gap-y-24'>
+          {quotes.map(item => (
+            <div className='shadow-custom rounded-lg p-4'>
+              <div className='relative head' style={{paddingLeft: iconWidth}}>
+                <img className='absolute top-0 left-0 transform -translate-y-1/2 icon-quote w-15 lg:w-15' src='/icons/icon-quote.png' />
+                <h4 className='font-bold'>
+                  {item.name}
+                <span className='block font-light text-16'>age {item.age}</span>
                 </h4>
-              </Link>
+              </div>
+              <div className='my-4'>
+                <h4 className='mb-4'>{item.text1}</h4>
+                <h4 className='mb-4'>{item.text2}</h4>
+                <Link href='/whatdootherssay/[id]' as={`/whatdootherssay/${item.id}`}>
+                  <h4 className='text-customTeal-500 hover:text-customYellow-500 cursor-pointer'>
+                    Read More
+                    <FontAwesomeIcon className='ml-2' icon={['fas', 'long-arrow-alt-right']} />
+                  </h4>
+                </Link>
+              </div>
             </div>
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
-    </div>
+      </section>
   )
 }
 
