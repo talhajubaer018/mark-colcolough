5ce9d31a8614dcd41779dd11dd9606131c2484ae
talhajubaer018 <52033279+talhajubaer018@users.noreply.github.com>
2021-10-22 23:35:44 +0600

    header fixed

--- components/Header.js
+++ components/Header.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 
+import Link from 'next/dist/client/link'
+
 import Navbar from './Navbar'
 
 const Header = () => {
@@ -17,9 +19,11 @@ const Header = () => {
 
   return (
     <header className='container mx-auto flex place-items-center py-4'>
-      <div className='mr-auto'>
-        <img className='w-3/5' src='/images/logo.png' />
-      </div>
+      <Link href='/'>
+        <div className='mr-auto cursor-pointer'>
+          <img className='w-3/5' src='/images/logo.png' />
+        </div>
+      </Link>
       <Navbar navItems={navItems}/>
 
     </header>
