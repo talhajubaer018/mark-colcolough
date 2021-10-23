6274950286263368f4bf3a2e7d0c1de8416f26fa
talhajubaer018 <52033279+talhajubaer018@users.noreply.github.com>
2021-10-23 11:12:54 +0600

    meet mark mobile fix

--- pages/meetMark.js
+++ pages/meetMark.js
@@ -13,10 +13,10 @@ const meetmark = ({  }) => {
   }, [])
 
   return (
-    <section className='py-8'>
+    <section className='py-8 w-11/12 sm:w-full mx-auto'>
       <div className='container mx-auto'>
-        <h1 className='text-center mb-12 text-customYellow-500'>Meet Mark</h1>
-        <div className='grid grid-cols-2 gap-x-8'>
+        <h1 className='text-center mb-4 sm:mb-12 text-customYellow-500'>Meet Mark</h1>
+        <div className='grid grid-cols-1 sm:grid-cols-2 gap-y-8 sm:gap-y-0 gap-x-0 sm:gap-x-8'>
           <div>
             <img src='/images/mark.png' />
           </div>
@@ -35,9 +35,9 @@ const meetmark = ({  }) => {
               <p className='mb-4'>Are you in a situation that you need help solving?</p>
               <p>I offer you a safe setting, in complete confidence, from where you can explore your thoughts, feelings and experiences. My practice encompasses individual psychotherapy and couples counselling. I have two clinics, one in Svendborg and the other centrally located in downtown Copenhagen. My professional background and extensive experience will support you in your personal development.</p>
             </div>
-            <div className='flex place-items-center justify-evenly mt-12 mb-4'>
-              <img className='w-1/5' src='/images/efpp-logo.png' alt='efpp-logo' />
-              <img className='w-1/5' src='/images/ukcp-logo.png' alt='ukcp-logo' />
+            <div className='flex place-items-center justify-evenly mt-4 sm:mt-12 mb-4'>
+              <img className='w-1/3 sm:w-1/5' src='/images/efpp-logo.png' alt='efpp-logo' />
+              <img className='w-1/3 sm:w-1/5' src='/images/ukcp-logo.png' alt='ukcp-logo' />
             </div>
           </div>
         </div>
