a15effa90425d3985eb940d8225131e32bfb5dc8
talhajubaer018 <52033279+talhajubaer018@users.noreply.github.com>
2021-10-23 10:56:33 +0600

    footer mobile fix

--- components/Footer.js
+++ components/Footer.js
@@ -2,10 +2,13 @@ import React from 'react'
 import Link from 'next/link'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
+import SocialIcons from './SocialIcons'
+
 const Footer = () => {
   return (
     <footer>
-      <div className='grid grid-cols-3 place-items-center bg-customTeal-200 py-8'>
+      <div className='flex flex-col sm:grid grid-cols-1 sm:grid-cols-3 place-items-center bg-customTeal-200 py-8'>
+        <SocialIcons propClass={'block sm:hidden'} />
         <div className='font-light text-center'>
           <h4 className='font-bold'>Mark Colcolough</h4>
           <h4>ShareThisRide ApS</h4>
@@ -17,19 +20,9 @@ const Footer = () => {
           <h4 className='mb-4 text-customTeal-500'>+4561488488</h4>
           <h4 className='text-customTeal-500'>hello@markcolclough.dk</h4>
         </div>
-        <div className='flex gap-x-8 text-customGray-700 fa-2x'>
-          <div>
-            <FontAwesomeIcon className='' icon={['fab', 'facebook']} />
-          </div>
-          <div>
-            <FontAwesomeIcon className='' icon={['fab', 'instagram']} />
-          </div>
-          <div>
-            <FontAwesomeIcon className='' icon={['fab', 'linkedin-in']} />
-          </div>
-        </div>
+        <SocialIcons propClass={'hidden sm:block'} />
       </div>
-      <div className='grid grid-cols-3 place-items-center bg-customTeal-200 pb-8'>
+      <div className='grid grid-cols-1 sm:grid-cols-3 place-items-center bg-customTeal-200 pb-8'>
         <h6>© Copyright, 2021</h6>
         <h6></h6>
         <Link href='/privacy'>
